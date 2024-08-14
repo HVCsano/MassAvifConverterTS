6be1457933c88df54b0b826cbe8072e49f5bebb2
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-08-14 23:45:07 +0200

    initial

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client'
+import { $ } from 'bun'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    let cuccok = await prisma.data.findMany()
+    for(const cucc of cuccok){
+        let darab = cucc.kep.split(".")
+        let name:string[] = []
+        for (let i = 0; i < darab.length; i++) {
+            if(i !== darab.length-1){
+                name.push(darab[i])
+            }
+            
+        }
+        let filename = name.toString().replaceAll(",",".")
+        await $`ffmpeg -i ./muti/${cucc.kep} ./muti/${filename}.avif`
+    }
+}
+main()
