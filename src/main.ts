3e0bf2c93862891b4cbe33053409c33ac29a4c82
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-08-14 23:51:32 +0200

    added support

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client'
 import { $ } from 'bun'
 
+let supportedExt = ["png","jpg","jpeg"]
+
 const prisma = new PrismaClient()
 
 async function main() {
@@ -8,12 +10,16 @@ async function main() {
     for(const cucc of cuccok){
         let darab = cucc.kep.split(".")
         let name:string[] = []
+        let ext = ""
         for (let i = 0; i < darab.length; i++) {
             if(i !== darab.length-1){
                 name.push(darab[i])
+            }else{
+                ext = darab[i]
             }
             
         }
+        if(!supportedExt.includes(ext)) break;
         let filename = name.toString().replaceAll(",",".")
         await $`ffmpeg -i ${Bun.env.dir}/${cucc.kep} ${Bun.env.dir}/${filename}.avif`
     }
